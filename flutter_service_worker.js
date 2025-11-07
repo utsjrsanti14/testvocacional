efc747ab742275a5cd962777c64f9cdb390d3767
santiagoarti22 <135990759+santiagoarti22@users.noreply.github.com>
2025-11-07 16:16:07 -0600

    Built web
    Branch: main
    Commit: ee71e45175f32dc8e81c49e93f441572b3e07ca0
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f88583d810725b9c427c95c30552a069
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "5f28e27892d24a871614a4aeca381058",
+"flutter_bootstrap.js": "30b315ad950f3583d1cebc4f9aace6a0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
