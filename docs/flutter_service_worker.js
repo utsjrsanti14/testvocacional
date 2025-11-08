ab1a1402688655f4216d8bdedd3f712d1d6e8b1b
santiagoarti22 <135990759+santiagoarti22@users.noreply.github.com>
2025-11-07 23:44:26 -0600

    Corrige base href para GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f88583d810725b9c427c95c30552a069
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4e0dc8a7ca1084c2fb3c4a708adef149",
+"flutter_bootstrap.js": "66a9a5b452cb5232c6412e67ae310a48",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
